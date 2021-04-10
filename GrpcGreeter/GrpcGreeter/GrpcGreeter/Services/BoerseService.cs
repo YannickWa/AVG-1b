3c75e9311c92b951fc29aea1dade70f706f51b5c
YannickWa <yannick.walla.yw@gmail.com>
2021-04-10 11:45:58 +0200

    Initial commit

---
+++ GrpcGreeter/GrpcGreeter/GrpcGreeter/Services/BoerseService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcGreeter.Services
+{
+    public class BoerseService : Boerse.BoerseBase
+    {
+        private readonly ILogger<BoerseService> _logger;
+        public BoerseService(ILogger<BoerseService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<TradeInfo> GetTradeNr(TradeSuchenMitNr request, ServerCallContext context)
+        {
+            TradeInfo output = new TradeInfo();
+            if (request.Id == "1")
+            {
+                output.id = "1";
+                output.name = "Rast&Ruh";
+                output.menge = 1000;
+                output.datum = "2021, 01, 01, 07, 35, 00";
+            }
+            else if (request.Id == "2")
+            {
+                output.id = "2";
+                output.name = "BenzaAG";
+                output.menge = 420;
+                output.datum = "2021, 01, 01, 07, 40, 00";
+            }
+            else if (request.Id == "3")
+            {
+                output.id = "3";
+                output.name = "GmbH&KoKAG";
+                output.menge = 300;
+                output.datum = "2021, 01, 01, 08, 01, 00";
+            }
+
+            return Task.FromResult(output);
+        }
+    }
+}
---
+++ GrpcGreeter/GrpcGreeterClient/GrpcGreeterClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main()
+        {
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var boerseClient = new Boerse.BoerseClient(channel);
+
+            var tradeRequested = new TadeSuchenMitNr { id = "1" };
+
+            var trade = await BoerseClient.GetTadeInfoAsync(tradeRequested);
+
+            Console.WriteLine($" ID : {trade.id} \n Name : {trade.name} \n Anzahl : {trade.menge} \n Datum : {artikel.datum}");
+
+            Console.ReadKey();
+        }
+    }
+}
